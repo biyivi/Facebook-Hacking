c14e06a9d336aab18a738664b4a5c63decf8b068
biyivi <biyivi@users.noreply.github.com>
2021-04-24 14:05:23 -0500

    Facebook-Hacking

---
+++ Facebook-Hacking.sh
@@ -0,0 +1,57 @@
+#!bin/bash
+#by biyivi
+echo -e "\e[1;34;47m"
+echo " ___ _   ___ ___ ___  ___   ___  _  _ "
+echo "| __/_\ / __| __| _ )/ _ \ / _ \| |/ /"
+echo "| _/ _ \ (__| _|| _ \ (_) | (_) | ' < "
+sleep 2
+echo -e "\e[1;33;41m"
+echo "By bivivi"
+echo -e "\e[1;30;40m"
+echo ""
+echo ""
+echo ""
+sleep 2
+echo -e "\e[1;32;40m"
+echo "Bienvenido :)"
+echo "Hackea cuentas de Facebook solo con el correo de la victima :D"
+echo ""
+echo -e "\e[1;32m"
+echo "Presione ENTER para comenzar "
+read ENTER
+sleep 1
+echo ""
+echo -e "\e[1;33;40m"
+echo "💀💀Escribe el correo de la victima: "
+read ENTER
+echo -e "\e[1;34;40m"
+echo ""
+echo "👀Buscando Perfil en Facebook de la Victima....."
+sleep 3
+echo -e "\e[1;32;40m"
+echo ""
+echo "Perfil encontrado :D"
+sleep 3
+echo ""
+echo -e "\e[1;37;40m"
+echo "Presione ENTER para comenzar el ataque 😈"
+read ENTER
+sleep 3
+echo -e "\e[1;31;40m"
+echo ""
+echo ""
+echo -e "\e[1;31;42m"
+echo  ""
+echo "⚠️ ⚠️ SU DISPOSITIVO HA SIDO INFECTADO, GRACIAS!!!!! ⚠️ ⚠️  <3 🥰"
+sleep 1
+echo ""
+echo "🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮"
+echo "🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮"
+echo "" 
+sleep 2
+echo -e "\e[1;32;41m"
+echo ""
+yes 💣  💀 ⚠️ERROR 404⚠️  💀 ⚠️ERROR DE ANALISIS⚠️ 💀 👹OBTENIENDO INFORMACION👹 💀 🤑DATOS ROBADOS🤑 💀 👥DESCARGANDO CONTACTOS👥 💀 🚀CONTRASEÑAS GUARDADAS🚀 💀 🦠ENCRIPTANDO ARCHIVOS🦠 💀 📲OBTENIENDO FOTOS📲 💀 📱OBTENIENDO VIDEOS📱 💀 🤖CAMBIANDO CONTRASEÑAS🤖 💀 👁️ENVIANDO DOCUMENTOS👁️ 💀 🥰GRACIAS🥰💀☠️🔥🤮🤮🤮🤮🤮 🔥su dispositivo ha sido infectado ☠️ 🥵🥵🥵🥵🥵 🔥su dispositivo ha sido infectado🤮☠️👾  |head -n 10000000000
+sleep 3
+
+
